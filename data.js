dc0e47a3668d57302b9c91c279144b9e429f479e
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-03-21 15:34:45 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,23 @@
+export const posts = [
+{
+    id: 1,
+    heading: "Blog one",
+    text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    image: "https://images.unsplash.com/photo-1611348586755-53860f7ae57a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    date: "April 23, 2022"
+},
+{
+    id: 2,
+    heading: "Blog two",
+    text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    image: "https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+    date: "April 30, 2022"
+},
+{
+    id: 3,
+    heading: "Blog three",
+    text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    image: "https://images.unsplash.com/photo-1600195077909-46e573870d99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+    date: "MAY 3, 2022"
+},
+]
