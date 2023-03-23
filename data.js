9fd80b5b9f24537578b6bd4bba5e71850db64644
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-03-23 13:24:47 -0400

    created "about me" page, added mobile layout

--- data.js
+++ data.js
@@ -4,14 +4,14 @@ export const posts = [
     heading: "Blog one",
     text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
     image: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80",
-    date: "April 23, 2022"
+    date: "APRIL 23, 2022"
 },
 {
     id: 2,
     heading: "Blog two",
     text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
     image: "https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
-    date: "April 30, 2022"
+    date: "APRIL 30, 2022"
 },
 {
     id: 3,
